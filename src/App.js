bab4c2efd4c2150edc9944aabf3ab20186887611
20211144 <gkskdml7419@gmail.com>
2024-12-12 13:55:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react'
+import './App.css';
+
+const App = () => {
+  const [message,setMessage] = useState('서버 접속 중...')
+
+  const fetchData = async () => {
+    try{
+      const apiUrl = process.env.REACT_APP_API_URL
+
+      const response = await fetch(apiUrl)
+
+      if(!response.ok){
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+
+      setMessage(data.status)
+    } catch (error){
+      console.error('Error fetching data: ', error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>취미 생활 데이터</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
